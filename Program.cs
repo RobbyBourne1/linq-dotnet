72293153052f357cad18aa3fc89305ffcefd61b1
Robby Bourne <robbybourne1@gmail.com>
2017-08-22 13:37:06 -0400

    Initial SetUp

--- Program.cs
+++ Program.cs
@@ -8,5 +8,23 @@ namespace linq_dotnet
         {
             Console.WriteLine("Hello World!");
         }
+
+    // public static int sumOfEarthShips (List<Spaceship> ships) {
+    //     return -1;
+    // }
+
+    // public static void sendShipsToFightMonsters (List<Spaceship> ships) {
+
+    // }
+
+    // public static Spaceship findMostGunsWithSize(List<Spaceship> ships, int size) {
+    //     return null;
+    // }
+
+    // public static IEnmerable<Spaceship> ExplorationFleet(List<Spaceship> ships) {
+    //     return null;
+    // }
+    // 
     }
 }
+
