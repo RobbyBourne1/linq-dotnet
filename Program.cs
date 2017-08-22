0feac5b1604e220c8f75af7666a05b0a50b44669
Robby Bourne <robbybourne1@gmail.com>
2017-08-22 13:41:22 -0400

    Set Up Finalized

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 
-namespace linq_dotnet
+namespace Spaceship
 {
     class Program
     {
@@ -9,22 +10,22 @@ namespace linq_dotnet
             Console.WriteLine("Hello World!");
         }
 
-    // public static int sumOfEarthShips (List<Spaceship> ships) {
-    //     return -1;
-    // }
+    public static int sumOfEarthShips (List<Spaceship> ships) {
+        return -1;
+    }
 
-    // public static void sendShipsToFightMonsters (List<Spaceship> ships) {
+    public static void sendShipsToFightMonsters (List<Spaceship> ships) {
 
-    // }
+    }
 
-    // public static Spaceship findMostGunsWithSize(List<Spaceship> ships, int size) {
-    //     return null;
-    // }
+    public static Spaceship findMostGunsWithSize(List<Spaceship> ships, int size) {
+        return null;
+    }
 
-    // public static IEnmerable<Spaceship> ExplorationFleet(List<Spaceship> ships) {
-    //     return null;
-    // }
-    // 
+    public static IEnumerable<Spaceship> ExplorationFleet(List<Spaceship> ships) {
+        return null;
+    }
+    
     }
 }
 
